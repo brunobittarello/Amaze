4b5cf0971607a5780b0ad97b34be29f9a4a9dc32
Bruno Bittarello <brunobitta@hotmail.com>
2024-10-29 10:28:10 -0300

    Camera angle and loading in the right axis

--- Amaze/Assets/Scripts/GameplayBehaviour.cs
+++ Amaze/Assets/Scripts/GameplayBehaviour.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class GameplayBehaviour : MonoBehaviour
 {
     const int POOL_SIZE = 50;
+
+    const char WALL_ID = '1';
+    const char PLAYER_ID = '2';
 
     [SerializeField]
     private TextAsset stageMap;
@@ -16,6 +20,9 @@ public class GameplayBehaviour : MonoBehaviour
 
     private Transform _wallPool;
     private Transform _stage;
+    private int[][] _stageMap;
+    private Transform _player;
+    private Vector2Int _playerPos;
 
     // Start is called before the first frame update
     void Start()
@@ -30,14 +37,13 @@ public class GameplayBehaviour : MonoBehaviour
         CreatePool();
 
         var stageGrid = stageMap.text.Split('\n');
-        Debug.Log(stageGrid[0]);
-        LoadStage(stageGrid);
+        LoadStage(stageGrid, stageGrid[0].Length - 1, stageGrid.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        HandlePlayerMovement();
     }
 
     void CreatePool()
@@ -50,23 +56,60 @@ public class GameplayBehaviour : MonoBehaviour
         }
     }
 
-    void LoadStage(string[] mapGrid)
+    void LoadStage(string[] mapGrid, int maxX, int maxY)
     {
-        for (int i = 0; i < mapGrid.Length; i++)
+        _stageMap = new int[maxX][];
+        for (int i = 0; i < maxX; i++)
         {
-            for (int l = 0; l < mapGrid[i].Length; l++)
+            _stageMap[i] = new int[maxY];
+            for (int l = 0; l < maxY; l++)
             {
-                var itemId = mapGrid[i][l];
-                if (itemId == '1') {
+                var itemId = mapGrid[maxY - i - 1][l];
+                if (itemId == WALL_ID)
+                {
+                    _stageMap[i][l] = WALL_ID;
                     var wall = _wallPool.GetChild(0);
                     wall.parent = _stage;
-                    wall.localPosition = new Vector3(i, 0, l);
-                } else if (itemId == '1') {
-                    var player = Instantiate(PlayerPrefab).transform;
-                    player.parent = _stage;
-                    player.localPosition = new Vector3(i, 0, l);
+                    wall.localPosition = new Vector2(i, l);
+                }
+                else if (itemId == PLAYER_ID)
+                {
+                    _player = Instantiate(PlayerPrefab).transform;
+                    _player.parent = _stage;
+                    _player.localPosition = new Vector2(i, l);
+                    _playerPos = new Vector2Int(i, l);
                 }
             }
         }
+    }
+
+    void HandlePlayerMovement()
+    {
+        var movement = PlayerMovement();
+        if (movement == Vector2Int.zero)
+            return;
+
+        if (CanPlayerMove(movement))
+            Move(movement);
+    }
+
+    Vector2Int PlayerMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) return Vector2Int.up;
+        if (Input.GetKeyDown(KeyCode.S)) return Vector2Int.down;
+        if (Input.GetKeyDown(KeyCode.A)) return Vector2Int.left;
+        if (Input.GetKeyDown(KeyCode.D)) return Vector2Int.right;
+        return Vector2Int.zero;
+    }
+
+    bool CanPlayerMove(Vector2Int movement)
+    {
+        return _stageMap[_playerPos.x + movement.x][_playerPos.y + movement.y] != WALL_ID;
+    }
+
+    void Move(Vector2Int movement)
+    {
+        _playerPos += movement;
+        _player.localPosition = (Vector3Int)_playerPos;
     }
 }
